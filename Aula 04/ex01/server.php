e99ef4d3c35cf22d454a0b4753d64c05692c89e9
filipecode-03 <158044004+filipecode-03@users.noreply.github.com>
2024-11-28 11:10:52 -0400

    Como os arquivos adicionais de style.css não estão disponiveis irei interromper as operações desse curso

---
+++ Aula 04/ex01/server.php
@@ -0,0 +1,11 @@
+<?php 
+    $idade = 3;
+    $sal = 1825.54;
+    $nome = "Leonardo";
+    $casado = false;
+
+    echo "Minha idade é $idade";
+    echo "Meu salario é " + $sal;
+    echo "Meu nome é $nome";
+    echo "Eu sou casado? $casado";
+?>
